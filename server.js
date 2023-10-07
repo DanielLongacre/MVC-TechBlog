e53c883af1f30b30ba2c0d9c90ce2e505f0b822e
Daniel Longacre <danieljlongacre@gmail.com>
2023-10-07 10:54:26 -0400

    Added necessary directories and started server.js file

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+require('dotenv').config();
 
+const path = require('path');
+const express = require('express');
+const session = require('session');
+const exphbs = require('express-handlebars');
+const routes = require('');
